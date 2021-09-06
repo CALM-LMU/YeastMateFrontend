af71bcd8816a5c7bde12082a4de943667e6de659
David Bunk <davidolafbunk@gmail.com>
2021-09-06 15:34:46 +0200

    added tmpfile mac commands plus dependencies

--- app/callStuffMacOS.js
+++ app/callStuffMacOS.js
@@ -6,8 +6,12 @@ const runCommandTerminalMacOS = (command) => {
     var tempCommandFile = temp.openSync({suffix: ".command"});
     fs.writeFileSync(tempCommandFile.fd, command);
     fs.chmodSync(tempCommandFile.path, "700");
-    spawn('open', [tempCommandFile.path]);
+    proc = spawn('open', [tempCommandFile.path]);
     temp.cleanupSync();
+
+    return proc
 }
+
+module.exports = {runCommandTerminalMacOS: runCommandTerminalMacOS};
 
 // runCommandTerminalMacOS("man cp");
